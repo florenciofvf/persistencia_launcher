abc276f19bfe5f8af92917a43184ca1a0be51223
Florencio Vieira Filho <florenciovieira@gmail.com>
2024-11-15 07:19:47 -0300

    Atualização

--- src/br/com/persist/main/CLPersistenciaUtil.java
+++ src/br/com/persist/main/CLPersistenciaUtil.java
@@ -1,6 +1,9 @@
 package br.com.persist.main;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
@@ -25,7 +28,8 @@ public class CLPersistenciaUtil {
 	}
 
 	public static File[] getFiles() {
-		File[] files = new File("libs").listFiles();
+		File fileLibs = new File("libs");
+		File[] files = fileLibs.listFiles();
 		List<File> lista = new ArrayList<>();
 		if (files != null) {
 			for (File f : files) {
@@ -33,6 +37,21 @@ public class CLPersistenciaUtil {
 				if (s.endsWith(".jar")) {
 					lista.add(f);
 				}
+			}
+		}
+		File outrasLibs = new File(fileLibs, "outras_libs");
+		if (outrasLibs.isFile()) {
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(outrasLibs)))) {
+				String linha = br.readLine();
+				while (linha != null) {
+					linha = linha.trim();
+					if (linha.endsWith(".jar")) {
+						lista.add(new File(linha));
+					}
+					linha = br.readLine();
+				}
+			} catch (Exception e) {
+				//
 			}
 		}
 		return lista.toArray(new File[lista.size()]);
